a5eb93982215c9bd678ad85a5a33ac7f648b8f80
ShacharGanon <shacharpolakk@gmail.com>
2025-04-20 19:44:33 +0300

    BMAIL-9: Add CheckURL GoogleTest suite and update CMakeLists

---
+++ CheckURL-Tests.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+#include "BloomFilter.h"
+
+// Test for a URL that is in the blacklist.
+// Should return True in both checks → prints "true true\n"
+TEST(CheckURLTest, ExistingURL) {
+    BloomFilter bf(128, 1, 1);
+    std::string url = "www.test1.com";
+    bf.AddURL(url);
+    {
+        std::stringstream buffer;
+        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+        bf.CheckURL(url);
+
+        std::cout.rdbuf(old);
+
+        EXPECT_EQ(buffer.str(), "true true\n");
+    }
+}
+
+// Test for a URL that is not exist blacklist.
+// Should return False in the first check → prints "false\n
+TEST(CheckURLTest, NonExistingURL){
+    BloomFilter bf(128, 1, 1);
+    std::string url = "www.test2.com";
+    {
+        std::stringstream buffer;
+        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+        bf.CheckURL(url);
+
+        std::cout.rdbuf(old);
+
+        EXPECT_EQ(buffer.str(), "false\n");
+    } 
+}
+// Test for a URL that is FalsePositive.
+// First check returns true, second check (blacklist) fails → prints "true\nfalse\n"
+TEST(CheckURLTest, FalsePositive){
+    BloomFilter bf(8, 2, 1);
+    std::string real_url = "www.real.com";
+    std::string false_positive_url = "www.fake7.com";
+   
+    std::hash<std::string> h;
+    int index = h(real_url) % 8;
+    bf.getBitArray()[index] = 1;
+    bf.getBlacklist().insert(real_url);
+
+    {
+        std::stringstream buffer;
+        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+        bf.CheckURL(real_url);
+
+        std::cout.rdbuf(old);
+        EXPECT_EQ(buffer.str(), "true true\n");
+    }
+
+    int index_fake = h(false_positive_url) % 8;
+    EXPECT_EQ(index_fake, index);
+
+    {
+        std::stringstream buffer;
+        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+        bf.CheckURL(false_positive_url);
+
+        std::cout.rdbuf(old);
+
+        EXPECT_EQ(buffer.str(), "true false\n");
+    } 
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
