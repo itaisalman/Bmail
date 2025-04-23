3f8963685088641a76106f951afbdb83fc25499b
ShacharGanon <shacharpolakk@gmail.com>
2025-04-23 16:14:23 +0300

    Update the False Negative test

--- CheckURL-Tests.cpp
+++ CheckURL-Tests.cpp
@@ -3,13 +3,14 @@
 
 // Test for a URL that is in the blacklist.
 // Should return True in both checks → prints "true true\n"
-TEST(CheckURLTest, ExistingURL) {
+TEST(CheckURLTest, ExistingURL)
+{
     BloomFilter bf(128, 1, 1);
     std::string url = "www.test1.com";
     bf.addUrl(url);
     {
         std::stringstream buffer;
-        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+        std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
 
         bf.checkUrl(url);
 
@@ -21,38 +22,45 @@ TEST(CheckURLTest, ExistingURL) {
 
 // Test for a URL that is not exist blacklist.
 // Should return False in the first check → prints "false\n
-TEST(CheckURLTest, NonExistingURL){
+TEST(CheckURLTest, NonExistingURL)
+{
     BloomFilter bf(128, 1, 1);
     std::string url = "www.test2.com";
     {
         std::stringstream buffer;
-        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+        std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
 
         bf.checkUrl(url);
 
         std::cout.rdbuf(old);
 
         EXPECT_EQ(buffer.str(), "false\n");
-    } 
+    }
 }
 // Test for a URL that is FalsePositive.
 // First check returns true, second check (blacklist) fails → prints "true\nfalse\n"
-TEST(CheckURLTest, FalsePositive){
+TEST(CheckURLTest, FalsePositive)
+{
     BloomFilter bf(8, 2, 1);
-    std::string real_url = "www.real.com";
-    std::string false_positive_url = "www.fake7.com";
-   
-    hash_funcs = bf.getHashFuncVector();
-    h1 = hash_funcs[0].first;
-    int index = h1(real_url) % 8;
+    std::string real_url = "www.real8.com";
+    std::string false_positive_url = "www.fake6.com";
 
-    bf.getBitArray()[index] = 1;
+    auto hash_funcs = bf.getHashFuncVector();
+
+    auto h1 = hash_funcs[0].first;
+    auto h2 = hash_funcs[1].first;
+
+    int index_h1 = h1(std::to_string(h1(real_url))) % bf.getSize();
+    int index_h2 = h2(real_url) % bf.getSize();
+
+    bf.getBitArray()[index_h1] = 1;
+    bf.getBitArray()[index_h2] = 1;
 
     bf.getBlacklist().insert(real_url);
 
     {
         std::stringstream buffer;
-        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+        std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
 
         bf.checkUrl(real_url);
 
@@ -60,22 +68,26 @@ TEST(CheckURLTest, FalsePositive){
         EXPECT_EQ(buffer.str(), "true true\n");
     }
 
-    int index_fake = h1(false_positive_url) % 8;
-    EXPECT_EQ(index_fake, index);
+    int index_f1 = h1(std::to_string(h1(false_positive_url))) % bf.getSize();
+    int index_f2 = h2(false_positive_url) % bf.getSize();
+
+    EXPECT_EQ(index_h1, index_f1);
+    EXPECT_EQ(index_h2, index_f2);
 
     {
         std::stringstream buffer;
-        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+        std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
 
         bf.checkUrl(false_positive_url);
 
         std::cout.rdbuf(old);
 
         EXPECT_EQ(buffer.str(), "true false\n");
-    } 
+    }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
