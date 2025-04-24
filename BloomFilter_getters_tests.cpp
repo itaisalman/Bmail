adea650f6afc243628a7752b9ef5a929223b2472
Omergolan11 <omergolan2001@gmail.com>
2025-04-24 18:48:18 +0300

    Updated the test to match the new constructor due to a misunderstanding of the requirements.

--- BloomFilter_getters_tests.cpp
+++ BloomFilter_getters_tests.cpp
@@ -4,21 +4,23 @@
 using namespace std;
 
 // Creating an instance and check if succeeded by comparing expected values to the fields.
-TEST(BloomFilterTests, getters) 
+TEST(BloomFilterTests, getters)
 {
-    BloomFilter bf(8, 2, 1);
+    vector<int> times_vector = {2, 1, 3};
+    BloomFilter bf(8, times_vector);
     EXPECT_NE(bf.getBitArray(), nullptr);
     EXPECT_EQ(bf.getSize(), 8);
-    for (int i = 0; i < bf.getSize(); i++) 
+    for (int i = 0; i < bf.getSize(); i++)
     {
         EXPECT_EQ(bf.getBitArray()[i], 0);
     }
-    ASSERT_EQ(bf.getHashFuncVector().size(), 2);
+    ASSERT_EQ(bf.getHashFuncVector().size(), 3);
     EXPECT_EQ(bf.getHashFuncVector()[0].second, 2);
-    EXPECT_EQ(bf.getHashFuncVector()[1].second, 1);       
+    EXPECT_EQ(bf.getHashFuncVector()[1].second, 1);
+    EXPECT_EQ(bf.getHashFuncVector()[2].second, 3);
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
