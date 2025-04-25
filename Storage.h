d5cd9aef18beffb984a42151fa0efab5d79b4672
Itai_Salman <itai.salman3@gmail.com>
2025-04-25 18:18:14 +0300

    Built the load urls list function, a header file as well.

---
+++ Storage.h
@@ -0,0 +1,12 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "BloomFilter.h"
+
+std::unordered_set<std::string> loadFromFile(const std::string &filename);
+void saveToFile(std::string url, const std::string &file_name);
+
+#endif
