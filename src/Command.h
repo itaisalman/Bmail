d4120c0ae06c247ccb9c530eb3732b13c387f81b
ShacharGanon <shacharpolakk@gmail.com>
2025-05-01 19:14:30 +0300

    Update syntax in Command Abstract class

--- src/Command.h
+++ src/Command.h
@@ -3,11 +3,11 @@
 class Command
 {
 public:
-    // Pure virtual function that any class (like add, check, delete)
-    // will need to implement execute(), which performs its action on the bloom filter.
-    virtual void execute(const std::string &url, BloomFilter &bf);
+    // Pure virtual function that Add, Check, and Delete classes will need to implement execute(),
+    // which performs their actions on the bloom filter.
+    virtual void execute(const std::string &url, BloomFilter &bf) = 0;
 
-    // Virtual destructor for that the destruction of a derived object
+    // Virtual destructor for that the delete of a derived object
     // will be performed correctly even when deleting via a pointer to the base
     virtual ~Command() {}
 };
