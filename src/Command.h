06750f30a12bde29a823969579d5dbdcb4372c9a
ShacharGanon <shacharpolakk@gmail.com>
2025-05-01 14:28:15 +0300

    Implement Command abstrat class with virtual execute method

--- src/Command.h
+++ src/Command.h
@@ -6,6 +6,7 @@ public:
     // Pure virtual function that any class (like add, check, delete)
     // will need to implement execute(), which performs its action on the bloom filter.
     virtual void execute(const std::string &url, BloomFilter &bf);
+
     // Virtual destructor for that the destruction of a derived object
     // will be performed correctly even when deleting via a pointer to the base
     virtual ~Command() {}
