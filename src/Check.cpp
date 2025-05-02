913f566afa5493fc10eaa35728694a1a64258b25
ShacharGanon <shacharpolakk@gmail.com>
2025-05-02 16:15:50 +0300

    Updating the class according to the change in the signature of the checkUrl function

--- src/Check.cpp
+++ src/Check.cpp
@@ -1,10 +1,9 @@
 #include "Check.h"
 #include "BloomFilter.h"
-#include <iostream>
-#include <sstream>
 
 // The Check class execution function – responsible for checking if URL is in the blacklist
 void Check::execute(const std::string &url, BloomFilter &bf)
 {
-    bf.checkUrl(url);
+    std::string outputCheck = bf.checkUrl(url);
+    output(outputCheck);
 }
