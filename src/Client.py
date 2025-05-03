5fa2d6971f3df2c9b2ecf30361e77ec3e08a2739
Omergolan11 <omergolan2001@gmail.com>
2025-05-03 18:00:08 +0300

    add comments

--- src/Client.py
+++ src/Client.py
@@ -2,13 +2,16 @@ import socket
 import sys
 
 def main():
+    # Check for getting exactly 2 command line arguments(IP and port).
     if len(sys.argv) != 3:
         print("Usage: python client.py <server_ip> <port>")
         return
 
+    # Declare server IP and port.
     server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
 
+    # Try create connection and handle with failure.
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((server_ip, server_port))
@@ -16,6 +19,7 @@ def main():
         print(f"Connection failed: {e}")
         return
 
+    # Loop that gets request from user, sends it to server and recieves back response.
     msg = input("Message to send (quit for exit): ")
     while not msg == 'quit':
         sock.send(bytes(msg, 'utf-8'))
