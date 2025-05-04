d2fbff15e6d8f0b79282f3778b5dd466c727d364
Omergolan11 <omergolan2001@gmail.com>
2025-05-04 19:50:12 +0300

    modify loop and added handle ungraceful exit

--- src/Client.py
+++ src/Client.py
@@ -11,25 +11,36 @@ def main():
     server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
 
-    # Try create connection and handle with failure.
+    sock = None
+    # Try create connection and handle with failure and unordered exit.
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((server_ip, server_port))
+        print("Connected to server.")
+        
+        # Loop for communicating with server.
+        while True:
+            msg = input("Enter your request: ")
+            sock.sendall(msg.encode('utf-8'))
+            data = sock.recv(4096)
+            print("Server sent:", data.decode('utf-8'))
+
+    # Handle with termination.
+    except KeyboardInterrupt:
+        print("\nClient terminated with keyboard interrupt (Ctrl+C).")
+
+    # Handle with errors like connection failure.
     except Exception as e:
-        print(f"Connection failed: {e}")
-        return
+        print(f"An error occurred: {e}")
+        
+    # Performs graceful exit if the socket is still open
+    finally:
+        if sock:
+            try:
+                sock.close()
+                print("Socket closed.")
+            except Exception as close_error:
+                print(f"Failed to close socket: {close_error}")
 
-    # Loop that gets request from user, sends it to server and recieves back response.
-    msg = input("Message to send (quit for exit): ")
-    while not msg == 'quit':
-        sock.send(bytes(msg, 'utf-8'))
-        data = sock.recv(4096)
-        print("Server sent: ", data.decode('utf-8'))
-        msg = input("Message to send (quit for exit): ")
-
-   # Close socket 
-    sock.close()
-
-# Call for main
 if __name__ == "__main__":
     main()
