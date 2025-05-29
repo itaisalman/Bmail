c9f2a69ae2356895c319e784f9bcbec59088cd4e
Itai_Salman <itai.salman3@gmail.com>
2025-05-29 15:08:56 +0300

    Fixed error output.

--- src/controllers/mails.js
+++ src/controllers/mails.js
@@ -4,12 +4,12 @@ exports.getFiftyMails = (req, res) => {
     const user_id = parseInt(req.headers['user']);
     // Check if user_id is given.
     if (!user_id){
-        return res.status(404).json({ error: 'Missing ID'})
+        return res.status(404).json({ error: 'Missing user ID'})
     }
     const user_mails = mails.getFiftyMails(user_id);
     // Check if the user_id given exists.
     if (!user_mails){
-        return res.status(404).json({ error: 'User ID not found'})
+        return res.status(404).json({ error: 'User not found'})
     }
     res.json(user_mails);
 }
