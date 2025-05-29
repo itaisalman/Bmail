174eff6d32086aa2f511ae4354dfa6cd2267d9c2
ShacharGanon <shacharpolakk@gmail.com>
2025-05-29 12:31:37 +0300

    Add PATCH /api/labels/:id functionality with validation

--- src/controllers/labels.js
+++ src/controllers/labels.js
@@ -51,3 +51,32 @@ exports.createLabel = (req, res) => {
   }
   res.status(201).location(`/api/labels/${new_label.id}`).end();
 };
+
+exports.updateLabel = (req, res) => {
+  const user_id = parseInt(req.headers["user"]);
+  // Check if user ID is missing
+  if (!user_id) {
+    return res.status(400).json({ error: "Missing user ID" });
+  }
+  const update_name = req.body;
+  const label_id = parseInt(req.params.id);
+  const updated_label = labels.updateLabel(user_id, label_id, update_name);
+  // If the user is not found
+  if (updated_label === null)
+    return res.status(404).json({ error: "User not found" });
+  // Validation checks if there is no body or the Name field does not exist
+  //  and if the name is empty or contains only spaces
+  if (!update_name || !update_name.name || update_name.name.trim() === "") {
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  // Check if the label ID is missing
+  if (label_id === undefined) {
+    return res.status(400).json({ error: "Missing label ID" });
+  }
+
+  // If the label is not found
+  if (updated_label === undefined)
+    return res.status(404).json({ error: "Label not found" });
+  res.status(204).send();
+};
