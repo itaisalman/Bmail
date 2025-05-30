33a25d3f2f19672baa402a7d05a3f73d08a8b224
Omergolan11 <omergolan2001@gmail.com>
2025-05-30 14:41:42 +0300

    seperation of errors and syntax fix

--- src/controllers/blacklist.js
+++ src/controllers/blacklist.js
@@ -1,11 +1,16 @@
 const blacklist = require("../models/blacklist");
 const users = require("../models/users");
 
-// Checks if the user_id given is valid (is a number, and is existing)
+// Checks if got numeric argument
 function checkIfValid(user_id) {
-  if (!user_id) {
+  if (!user_id || isNaN(user_id)) {
     return false;
   }
+  return true;
+}
+
+// Check if the user id exists in the users list.
+function checkIfExist(user_id) {
   if (!users.getUserById(user_id)) {
     return false;
   }
@@ -23,18 +28,23 @@ function passRequestToServer(req, res) {
     }
     // return the string returned from BloomFilter servers
     return res.status(201).json({
-      Answer: response.trim(),
+      answer: response.trim(),
     });
   });
 }
 
 // Pass the wanted request to server and check for validation
 function handleBlacklistOperation(req, res, method) {
-  const user_id = parseInt(req.headers["user"]);
-  if (!checkIfValid(user_id)) {
+  // Check that got a number
+  if (!checkIfValid(req.headers["user"])) {
     return res
       .status(400)
       .json({ error: "Missing/Invalid user ID or User not found" });
+  }
+  // Check that given id is exist
+  const user_id = parseInt(req.headers["user"]);
+  if (!checkIfExist(user_id)) {
+    return res.status(404).json({ error: "User not found" });
   }
 
   const { url } = req.body;
