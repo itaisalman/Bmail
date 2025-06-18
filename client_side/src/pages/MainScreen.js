331581410b1368cc245d201ca51fabcdf755e2f5
yael-bod <bodor.yael@gmail.com>
2025-06-18 19:51:10 +0300

    Fixed Comments

--- client_side/src/pages/MainScreen.js
+++ client_side/src/pages/MainScreen.js
@@ -1,20 +1,3 @@
-// import { Outlet } from "react-router-dom";
-// import Sidebar from "../components/Sidebar/Sidebar";
-// import "./MainScreen.css";
-
-// function MainScreen() {
-//   return (
-//     <div className="main-container">
-//       <Sidebar />
-//       <main className="main-content">
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default MainScreen;
-
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
