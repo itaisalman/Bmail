f053d6da9ca2d0161b2f41c7c96295068f5b6246
itaisalman <itai.salman3@gmail.com>
2025-06-18 20:04:58 +0300

    Create New Mail box added. (#164)
    * Create New Mail box added.
    
    * Fixed Comments
    
    ---------
    
    Co-authored-by: yael-bod <bodor.yael@gmail.com>

--- client_side/src/pages/MainScreen.js
+++ client_side/src/pages/MainScreen.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
+import MailComposer from "../components/MailComposer/MailComposer";
 import "./MainScreen.css";
 
 function MainScreen() {
+  const [showComposer, setShowComposer] = useState(false);
+
+  const toggleComposer = () => {
+    setShowComposer((prev) => !prev);
+  };
+
   return (
     <div className="main-container">
-      <Sidebar />
+      <Sidebar onNewMailClick={toggleComposer} />
       <main className="main-content">
         <Outlet />
       </main>
+      {showComposer && <MailComposer onClose={toggleComposer} />}
     </div>
   );
 }
