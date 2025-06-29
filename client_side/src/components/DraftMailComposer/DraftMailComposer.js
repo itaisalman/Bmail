285c655341380a6bb5532c5aa1b86b21fca49bdf
itaisalman <itai.salman3@gmail.com>
2025-06-29 20:09:03 +0300

    Fix redundancy

--- client_side/src/components/DraftMailComposer/DraftMailComposer.js
+++ client_side/src/components/DraftMailComposer/DraftMailComposer.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import MailComposer from "../MailComposer/MailComposer";
 import { useOutletContext } from "react-router-dom";
 
