de6f1429d0fc0018150b9955f2b6ed4650da6c0d
Omergolan11 <omergolan2001@gmail.com>
2025-07-03 00:16:06 +0300

    updated controller

--- src/controllers/tokens.js
+++ src/controllers/tokens.js
@@ -1,7 +1,7 @@
-const tokens = require("../models/tokens");
+const tokens = require("../services/tokens");
 const jwt = require("jsonwebtoken");
 
-exports.returnUserId = (req, res) => {
+exports.returnUserId = async (req, res) => {
   // Extract username and password from body
   const { username, password } = req.body;
 
@@ -10,17 +10,23 @@ exports.returnUserId = (req, res) => {
     return res.status(400).json({ error: "Missing username or password" });
   }
 
-  // Check that email address and password are existed
-  const user = tokens.findUserByUsernameAndPassword(
-    username + "@bmail.com",
-    password
-  );
+  try {
+    // Check that email address and password are existed
+    const email = username.endsWith("@bmail.com")
+      ? username
+      : username + "@bmail.com";
 
-  // handle user not found
-  if (!user) {
-    return res.status(404).json({ error: "User Not Found" });
+    const user = await tokens.findUserByUsernameAndPassword(email, password);
+
+    // handle user not found
+    if (!user) {
+      return res.status(404).json({ error: "User Not Found" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.TOKEN);
+    // return user id
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.error("Error generating token:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-  const token = jwt.sign({ id: user.id }, process.env.TOKEN);
-  // return user id
-  res.json({ token });
 };
