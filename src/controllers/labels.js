3f95d378cfee00809903ed97da36f48da414a7f9
ShacharGanon <shacharpolakk@gmail.com>
2025-07-03 14:03:47 +0300

    Changing controllers/labels according to Label schema

--- src/controllers/labels.js
+++ src/controllers/labels.js
@@ -1,12 +1,51 @@
-const users = require("../models/users");
-const labels = require("../models/labels");
+const mongoose = require("mongoose");
+const labelService = require("../services/labels");
+const userService = require("../services/users");
 
-// Checks if the id given is valid
-function isValidId(user_id) {
-  return /^\d+$/.test(user_id);
+// Checks if got valid ObjectId for the user.
+function checkIfValid(user_id) {
+  return mongoose.Types.ObjectId.isValid(user_id);
 }
 
-function isValidName(name, res) {
+const getValidatedUserId = (req) => {
+  const user_id = req.headers["user"];
+  if (!checkIfValid(user_id)) {
+    return null;
+  }
+
+  return user_id;
+};
+
+const DEFAULT_LABEL_NAMES = [
+  "inbox",
+  "starred",
+  "star",
+  "important",
+  "sent",
+  "drafts",
+  "draft",
+  "trash",
+  "spam",
+  "labels",
+];
+
+const isDuplicateLabelName = (labels, name, ignoreId = null) => {
+  const update_name = name.toLowerCase().trim();
+  if (!update_name) return true;
+
+  if (DEFAULT_LABEL_NAMES.includes(update_name)) return true;
+
+  return labels.some((label) => {
+    if (ignoreId && label._id.toString() === ignoreId.toString()) return false;
+
+    const currentName = label.name.toLowerCase().trim();
+    const sameName = currentName === update_name;
+
+    return sameName;
+  });
+};
+
+const isValidName = (name) => {
   // Check if name was not defined at all
   if (name === undefined || name === null) {
     return { status: 400, message: "Name is required" };
@@ -21,171 +60,197 @@ function isValidName(name, res) {
     return { status: 400, message: "Name is too long" };
   }
   return null;
-}
-
-const getValidatedUserId = (req, res) => {
-  // Extract the user ID from the headers
-  const user_id = req.headers["user"];
-  if (!isValidId(user_id)) {
-    res.status(400).json({ error: "Missing/Invalid user ID" });
-    return null;
-  }
-  // If the user's id is valid
-  return Number(user_id);
 };
 
-exports.getAllLabels = (req, res) => {
-  const user_id = getValidatedUserId(req, res);
-  if (user_id === null) return;
-  // Gets all the user's labels by ID
-  const userLabels = labels.getAllLabels(user_id);
-  if (!userLabels) {
-    return res.status(404).json({ error: "User not found" });
+exports.getAllUserLabels = async (req, res) => {
+  try {
+    const user_id = getValidatedUserId(req);
+    if (user_id === null) {
+      return res.status(400).json({ error: "Missing/Invalid user ID" });
+    }
+
+    const userLabels = await labelService.getAllUserLabelIds(user_id);
+
+    if (userLabels.length === 0) {
+      return res.status(404).json({ error: "User not found or has no labels" });
+    }
+
+    res.status(200).json(userLabels);
+  } catch (err) {
+    console.error("Error in getAllLabels:", err);
+    res.status(500).json({ error: "Server error while getting all labels" });
   }
-  res.json(userLabels);
 };
 
-exports.getLabelById = (req, res) => {
-  const user_id = getValidatedUserId(req, res);
-  if (user_id === null) return;
+exports.getLabelById = async (req, res) => {
+  try {
+    const { label_id } = req.params;
+    if (!checkIfValid(label_id)) {
+      return res.status(400).json({ error: "Invalid label ID" });
+    }
+    const label = await labelService.getLabelById(label_id);
 
-  const label_id = req.params.id;
-  const label = labels.getLabel(user_id, Number(label_id));
-  // If getLabel function return null- the user does not exist.
-  if (label === null) {
-    return res.status(404).json({ error: "User not found" });
-  }
+    if (!label) {
+      return res.status(404).json({ error: "Label not found" });
+    }
 
-  if (!isValidId(label_id)) {
-    return res.status(400).json({ error: "Missing/Invalid label ID" });
+    res.status(200).json(label);
+  } catch (err) {
+    console.error("Error in get getLabelById:", err);
+    res.status(500).json({ error: "Server error while getting label by Id" });
   }
-  // If the function return undefined- this means that the user exists,
-  // but no label with the received ID was found.
-  if (label === undefined) {
-    return res.status(404).json({ error: "Label not found" });
-  }
-  res.json(label);
 };
 
-exports.createLabel = (req, res) => {
-  const user_id = getValidatedUserId(req, res);
-  if (user_id === null) return;
+exports.createLabel = async (req, res) => {
+  try {
+    const user_id = getValidatedUserId(req);
+    if (user_id === null) {
+      return res.status(400).json({ error: "Missing/Invalid user ID" });
+    }
 
-  const { name } = req.body;
-  const validate_error = isValidName(name);
-  if (validate_error)
-    return res
-      .status(validate_error.status)
-      .json({ error: validate_error.message });
+    const { name } = req.body;
 
-  // Creates a new label for the user
-  const new_label = labels.createLabel(user_id, name);
-  if (new_label === null) {
-    return res.status(404).json({ error: "User not found" });
+    const nameError = isValidName(name);
+    if (nameError) {
+      return res.status(nameError.status).json({ error: nameError.message });
+    }
+
+    const user = await userService.getUserById(user_id);
+    if (isDuplicateLabelName(user.labels, name))
+      return res.status(409).json({
+        error:
+          "The label name you selected already exists. Try a different name",
+      });
+
+    const label = await labelService.createLabel(user_id, name);
+    res.status(201).json(label);
+  } catch (err) {
+    console.error("Error in createLabel:", err);
+    res.status(500).json({ error: "Server error while creating label" });
   }
-  if (new_label === undefined) {
-    return res.status(409).json({
-      error: "The label name you selected already exists. Try a different name",
-    });
-  }
-
-  res.status(201).location(`/api/labels/${new_label.id}`).end();
 };
 
-exports.updateLabel = (req, res) => {
-  // Gets and validates the user ID from the headers
-  const user_id = getValidatedUserId(req, res);
-  if (user_id === null) return;
+exports.updateLabel = async (req, res) => {
+  try {
+    const user_id = getValidatedUserId(req);
+    if (user_id === null) {
+      return res.status(400).json({ error: "Missing/Invalid user ID" });
+    }
 
-  const label_id = req.params.id;
-  // Checks if the label ID is not a standard number
-  if (!isValidId(label_id))
-    return res.status(400).json({ error: "Missing/Invalid label ID" });
+    const label_id = req.params.id;
+    if (!checkIfValid(label_id) || !label_id) {
+      return res.status(400).json({ error: "Missing/Invalid label ID" });
+    }
 
-  const { name } = req.body;
-  const validate_error = isValidName(name);
-  if (validate_error)
-    return res
-      .status(validate_error.status)
-      .json({ error: validate_error.message });
+    const { name } = req.body;
 
-  //Trying to update the label with the new name, after removing spaces
-  const updated_label = labels.updateLabel(user_id, Number(label_id), name);
+    const user = await userService.getUserById(user_id);
+    if (isDuplicateLabelName(user.labels, name, label_id))
+      return res.status(409).json({
+        error:
+          "The label name you selected already exists. Try a different name",
+      });
 
-  // If the user is not found
-  if (updated_label === null)
-    return res.status(404).json({ error: "User not found" });
+    const updated_label = await labelService.updateLabel(label_id, name);
 
-  // Check if there is already a label with the same name except for the label itself
-  if (updated_label === "conflict") {
-    return res.status(409).json({
-      error: "The label name you selected already exists. Try a different name",
-    });
+    if (!updated_label.matchedCount) {
+      return res.status(404).json({ error: "Label not found" });
+    }
+
+    res.status(200).json(updated_label);
+  } catch (err) {
+    console.error("Error in updateLabel:", err);
+    res.status(500).json({ error: "Server error while updating label" });
   }
-  // If the label is not found
-  if (updated_label === undefined)
-    return res.status(404).json({ error: "Label not found" });
-
-  res.status(200).json(updated_label);
 };
 
-exports.deleteLabel = (req, res) => {
-  const user_id = getValidatedUserId(req, res);
-  if (user_id === null) return;
+exports.deleteLabel = async (req, res) => {
+  try {
+    const user_id = getValidatedUserId(req);
+    if (user_id === null) {
+      return res.status(400).json({ error: "Missing/Invalid user ID" });
+    }
 
-  const label_id = req.params.id;
-  if (!isValidId(label_id)) {
-    return res.status(400).json({ error: "Missing/Invalid label ID" });
+    const { label_id } = req.params;
+    if (!checkIfValid(label_id) || !label_id) {
+      return res.status(400).json({ error: "Missing/Invalid label ID" });
+    }
+
+    const result = await labelService.deleteLabel(label_id);
+    if (!result.deletedCount) {
+      return res.status(404).json({ error: "Label not found" });
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    console.error("Error in get deleteLabel:", err);
+    res.status(500).json({ error: "Server error while deleting label" });
   }
-
-  const user = users.getUserById(user_id);
-  const label = user.labels.find((l) => l.id === Number(label_id));
-
-  if (!label) return res.status(404).json({ error: "Label not found" });
-
-  // Extracts the array's emails before deletion
-  const labelMails = label.mails || [];
-
-  const deleted_label = labels.deleteLabel(user_id, Number(label_id));
-  // Check if the user ID does not exist
-  if (deleted_label === null)
-    return res.status(404).json({ error: "User not found" });
-
-  // Moves the label emails back to the inbox
-  labelMails.forEach((mail) => {
-    user.received_mails.push(mail);
-  });
-
-  res.status(204).send();
 };
 
-exports.removeMailFromLabel = ({ headers, params, body}, res) => {
-  const user_id = +headers.user;
-  const labelId = +body.labelId;
-  const mailId = +params.mail_id;
+exports.assignMailToLabel = async (req, res) => {
+  try {
+    const label_id = req.body.label_id;
+    const mail_id = req.body.mail_id;
 
-  const user = users.getUserById(user_id);
-  if (!user) return res.status(404).json({ error: "User not found" });
+    if (!checkIfValid(label_id) || !checkIfValid(mail_id)) {
+      return res.status(400).json({ error: "Invalid label/mail ID" });
+    }
 
-  const label = user.labels.find((l) => l.id === labelId);
-  if (!label) return res.status(404).json({ error: "Label not found" });
+    const success = await labelService.assignLabel(label_id, mail_id);
 
-  const success = labels.removeMail(labelId, mailId, user_id);
-  if (!success) {
-    return res.status(404).json({ error: "Label or mail not found" });
+    if (!success || success.matchedCount === 0) {
+      return res.status(404).json({ error: "Label or mail not found" });
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    console.error("Error in assignLabelToMail:", err);
+    res
+      .status(500)
+      .json({ error: "Server error while assigning label to mail" });
   }
-
-  res.sendStatus(204);
 };
 
-exports.getMailLabels = ({ headers, params }, res) => {
-  const user_id = +headers.user;
-  const mail_id = +params.mail_id;
+exports.getMailLabels = async (req, res) => {
+  try {
+    const mail_id = req.params.mail_id;
+    if (!checkIfValid(mail_id)) {
+      return res.status(400).json({ error: "Invalid mail ID" });
+    }
 
-  const result = labels.getSpecificMailLabels(user_id, mail_id);
-  if (!result) {
-    return res.status(404).json({ error: "User or mail not found" });
+    const labels = await labelService.getMailLabels(mail_id);
+
+    if (labels.length === 0) {
+      return res.status(404).json({ error: "No labels found for this mail" });
+    }
+
+    res.status(200).json(labels);
+  } catch (err) {
+    console.error("Error in getMailLabels:", err);
+    res.status(500).json({ error: "Server error while getting mail labels" });
   }
+};
 
-  res.status(200).json(result);
+exports.removeMailFromLabel = async (req, res) => {
+  try {
+    const label_id = req.body.label_id;
+    const mail_id = req.params.mail_id;
+
+    if (!checkIfValid(label_id) || !checkIfValid(mail_id)) {
+      return res.status(400).json({ error: "Invalid label/mail ID" });
+    }
+
+    const success = await labelService.removeMailFromLabel(label_id, mail_id);
+    if (!success || success.matchedCount === 0) {
+      return res.status(404).json({ error: "Label or mail not found" });
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    console.error("Error in removeMailFromLabel:", err);
+    res
+      .status(500)
+      .json({ error: "Server error while removing mail from label" });
+  }
 };
