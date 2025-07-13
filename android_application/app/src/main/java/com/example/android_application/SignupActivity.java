2feb4b7905b7f64848dc4708846b196f45339d6e
Omergolan11 <omergolan2001@gmail.com>
2025-07-13 21:50:08 +0300

    build the UI

--- android_application/app/src/main/java/com/example/android_application/SignupActivity.java
+++ android_application/app/src/main/java/com/example/android_application/SignupActivity.java
@@ -1,8 +1,6 @@
 package com.example.android_application;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,34 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button signupButton;
-    Button loginButton;
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        signupButton = findViewById(R.id.signUpButton);
-        loginButton = findViewById(R.id.loginButton);
-
-        signupButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-            startActivity(intent);
-        });
-
-
-        loginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
         });
     }
 }
